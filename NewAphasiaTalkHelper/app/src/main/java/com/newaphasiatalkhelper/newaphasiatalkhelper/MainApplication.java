0f438a0dc181e29366fa17a7541f5b82a6a917bd
blinkblinkk <blinkblinkk@hotmail.com>
2017-09-17 14:59:53 +0700

    no message

---
+++ NewAphasiaTalkHelper/app/src/main/java/com/newaphasiatalkhelper/newaphasiatalkhelper/MainApplication.java
@@ -0,0 +1,8 @@
+package com.newaphasiatalkhelper.newaphasiatalkhelper;
+
+/**
+ * Created by blinkblinkk on 9/17/2017 AD.
+ */
+
+public class MainApplication {
+}
--- NewAphasiaTalkHelper/app/src/main/java/com/newaphasiatalkhelper/newaphasiatalkhelper/activities/MainActivity.java
+++ NewAphasiaTalkHelper/app/src/main/java/com/newaphasiatalkhelper/newaphasiatalkhelper/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.newaphasiatalkhelper.newaphasiatalkhelper;
+package com.newaphasiatalkhelper.newaphasiatalkhelper.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.newaphasiatalkhelper.newaphasiatalkhelper.R;
 
 public class MainActivity extends AppCompatActivity {
 
