d5bc959200acf71b61676eb9dd8b92be56f72ed4
blinkblinkk <blinkblinkk@hotmail.com>
2017-09-17 16:10:41 +0700

    no message

--- NewAphasiaTalkHelper/app/src/main/java/com/newaphasiatalkhelper/newaphasiatalkhelper/activities/MainActivity.java
+++ NewAphasiaTalkHelper/app/src/main/java/com/newaphasiatalkhelper/newaphasiatalkhelper/activities/MainActivity.java
@@ -1,15 +1,59 @@
 package com.newaphasiatalkhelper.newaphasiatalkhelper.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.newaphasiatalkhelper.newaphasiatalkhelper.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    View viewIWant;
+    View viewIFeel;
+    View viewTypeText;
+    View viewFav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewIWant = findViewById(R.id.goto_i_want);
+        viewIFeel = findViewById(R.id.goto_i_feel);
+        viewTypeText = findViewById(R.id.goto_type_text);
+        viewFav = findViewById(R.id.goto_fav);
+
+        viewIWant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectItemActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        viewIFeel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectItemActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        viewTypeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TypeTextActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        viewFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FavoriteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ NewAphasiaTalkHelper/app/src/main/java/com/newaphasiatalkhelper/newaphasiatalkhelper/activities/TypeTextActivity.java
@@ -0,0 +1,15 @@
+package com.newaphasiatalkhelper.newaphasiatalkhelper.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.newaphasiatalkhelper.newaphasiatalkhelper.R;
+
+public class TypeTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_type_text);
+    }
+}
